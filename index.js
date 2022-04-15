5f104e179067811206898bdadf6a8e056402e06e
Elaanor <lol_34_34@hotmail.com>
2022-04-15 16:08:58 +0300

    TODO

---
+++ index.js
@@ -0,0 +1,43 @@
+const express = require("express")
+const mongoose = require("mongoose")
+const exphbs = require("express-handlebars")
+const todoRoutes = require('./routes/todos')
+
+
+
+const app = express()
+const PORT = 3000
+
+
+
+
+const hbs = exphbs.create({
+    defaultLayout: 'main',
+    extname: 'hbs'
+})
+
+app.engine('hbs',hbs.engine)
+app.set('view engine','hbs')
+app.set('views', 'views')
+
+app.use(todoRoutes)
+
+
+
+const conn = "mongodb+srv://admin:1q2w3e@cluster0.tufnw.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+
+
+async function start() {
+    try {
+        await mongoose.connect(conn,{
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        })
+        app.listen(PORT, ()=> console.log("Server has been started..."))
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+
+start()
